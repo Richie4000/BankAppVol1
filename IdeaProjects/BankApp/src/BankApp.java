ba3efcce285bcc7061a44200da514ce0ba69ff07
malik-111 <abdul.899@hotmail.com>
2022-08-30 19:08:53 +0100

    maliks changes

--- IdeaProjects/BankApp/src/BankApp.java
+++ IdeaProjects/BankApp/src/BankApp.java
@@ -1,29 +1,47 @@
-import java.io.Console;
 import java.util.Scanner;
 
 public class BankApp {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
         String password = "Bankingwithus01";
 
-        System.out.println("Enter Username");
-        String getUsername = reader.nextLine();
-
-        System.out.println("Welcome to the banking app " + getUsername);
-
-
-        System.out.println("Enter Password");
-        String getPassword = reader.nextLine();
+        //System.out.println("Enter Username");
+        String getUsername = usernameInput();
+        String getPassword = passwordInput();
 
 
-        if (getPassword.equals(password)) {
-            System.out.println("Loading...");
+        if (password.equals(getPassword)) {
+            System.out.println("You have been validated");
         }
         else {
-            System.out.println("Ah ah ah... This is the wrong password.");
+            System.out.println("Access denied");
         }
 
+    }
 
+    public static String usernameInput(){
+        Scanner u = new Scanner(System.in);
+        System.out.println("Enter a username");
+        String username = u.nextLine();
+
+        while (username.isEmpty()){
+            System.out.println("Enter a username");
+            username = u.nextLine();
+        }
+
+        System.out.println("Welcome to the banking app " + username);
+
+        return username;
+    }
+    public static String passwordInput(){
+        Scanner u = new Scanner(System.in);
+        System.out.println("Enter a password");
+        String password = u.nextLine();
+
+        while (password.isEmpty()){
+            System.out.println("Enter password");
+            password = u.nextLine();
+        }
+
+        return password;
     }
 }
