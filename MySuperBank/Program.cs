4a0e9a76cc486dcc66f000dfc1ae3737b1aea1ff
Richie Osagie <richie0914@gmail.com>
2022-10-07 14:36:22 +0100

    Richie's Commit

---
+++ MySuperBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Richie", 12500);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}.");
+
+            account.MakeWithdrawal(150, DateTime.Now, "Trainers");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            account.MakeWithdrawal(50, DateTime.Now, "Xbox Game");
+            Console.WriteLine(account.Balance);
+
+        }
+    }
+}
